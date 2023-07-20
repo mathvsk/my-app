93347c502350242be4f1c523a3e9a1eb5fc5c66d
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2023-07-19 21:48:12 -0300

    finished

---
+++ README.md
@@ -0,0 +1,31 @@
+<h1 align="center">Full-Stack with Next</h1>
+<p align="center">
+  Full stack application in Next.js for learning.
+  The application uses the edge computing paradigm, employing server actions with Next and serverless computing with Neon.
+</p>
+
+<div align="center">
+  
+  ![License](https://img.shields.io/github/license/mathvsk/todo-list.svg)
+  [![Generic badge](https://img.shields.io/badge/status-finished-98c610.svg)](https://shields.io/)
+</div>
+
+## Technologies
++ [Next.JS](https://nextjs.org/)
++ [Vercel](https://vercel.com/docs) => deploy
++ [Neon](https://neon.tech/docs/introduction) => serverless computing
++ [Drizzle](https://orm.drizzle.team/) => ORM for edge computing
+
+## How to use
+Clone the repository.
+#### Install the dependencies.
+```
+  npm i
+```
+#### Run the project.
+```
+  npm run dev
+```
+
+## Project
+<img src=".github/next-application.png" alt="">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,45 @@
-export default function Home() {
-  return <h1>hello world</h1>
+import { db } from '@/db'
+import { users } from '@/db/schema'
+import { revalidatePath } from 'next/cache'
+
+export default async function Home() {
+  const allUsers = await db.select().from(users)
+
+  async function addUser(data: FormData) {
+    'use server'
+
+    console.log(data.get('Name'))
+
+    const fullName = data.get('name')?.toString()
+    const phone = data.get('phone')?.toString()
+
+    await db.insert(users).values({
+      fullName,
+      phone,
+    })
+
+    revalidatePath('/')
+  }
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-6 bg-zinc-900 text-zinc-50">
+      <pre>{JSON.stringify(allUsers, null, 2)}</pre>
+      <form action={addUser} className="flex flex-col gap-3">
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          className="bg-zinc-800"
+        />
+        <input
+          type="number"
+          name="phone"
+          placeholder="Phone"
+          className="bg-zinc-800"
+        />
+
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  )
 }
